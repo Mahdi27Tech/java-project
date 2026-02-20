d75395706ce5004f312c6ea904a14c7929a61007
Mahdi <2025bcs113@std.must.ac.ug>
2026-02-20 11:44:46 +0300

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+package com.mycompany.calculator;
+
+/**
+ *
+ * @author mahdi
+ */
+
+import java.util.Scanner;
+
+public class Calculator {
+    static void operators(){
+        System.out.println("+, -, *, /, %");
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        
+        System.out.println("Enter first number: ");
+        int fnum = obj.nextInt();
+        //System.out.println(fnum);
+        
+        System.out.println("Enter second number: ");
+        int snum = obj.nextInt();
+        //System.out.println(snum);
+        
+        System.out.println("Choose an operator to use");
+        operators();
+        String op = obj.nextLine();
+        System.out.println(op);
+        
+        switch(op){
+            case "+":
+                System.out.println(fnum + snum);
+                break;
+            case "-":
+                System.out.println(fnum - snum);
+                break;
+            case "*":
+                System.out.println(fnum * snum);
+                break;
+            case "/":
+                System.out.println(fnum / snum);
+                break;
+            case "%":
+                System.out.println(fnum % snum);
+                break;
+            default:
+                System.out.println("Enter a valid opartor");
+        }
+        /*System.out.println("Enter Username: ");
+        
+        String userName = obj.nextLine();
+        System.out.println(userName);*/
+    }
+}
